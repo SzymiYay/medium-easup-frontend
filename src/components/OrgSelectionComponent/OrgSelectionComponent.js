af3ad1f296ad86a65c71f5783058496aed06dcc1
Maciexpol <empieniazek@student.agh.edu.pl>
2023-01-14 23:21:30 +0100

    Implemented usage of new hook

--- src/components/OrgSelectionComponent/OrgSelectionComponent.js
+++ src/components/OrgSelectionComponent/OrgSelectionComponent.js
@@ -1,36 +1,53 @@
 import React from "react";
-import { useState } from "react";
 import './OrgSelectionComponent.css'
-import data from '../../data/organizations.json'
+import axios from '../../apis/easup'
+import useAxios from '../../hooks/useAxios'
+import LoadingIcon from '../LoadingIcon/LoadingIcon'
 
 function OrganizationIcon(props) {
   return (
     <div className='organization-icon'>
-      <h1>{props.name}</h1>
-      <img src={props.photo} alt={props.name} />
+      <img src={props.photo} alt={props.name} className='logo'/>
+        <h2>{props.name}</h2>
     </div>
   );
 }
 
 function OrgSelectionComponent() {
-  let [organizations, setOrganizations] = useState(data);
+  const [organizations, error, loading] = useAxios({
+      axiosInstance: axios,
+      method: 'GET',
+      url: '/organizations/user/1',
+      requestConfig: {}
+  })
 
   return (
     <div>
       <div className='org-selection-component'>
-        <h1>Organizations</h1>
-        <ul>
-          {organizations.map((orgItem, index) => {
-            return (
-              <OrganizationIcon
-                key={index}
-                id={index}
-                name={orgItem.name}
-                photo={orgItem.photo}
-              />
-            );
-          })}
-        </ul>
+
+          {loading && <LoadingIcon/>}
+
+          {!loading && error && <h1>{error}</h1>}
+
+          {!loading && !error && !organizations && <h1>Something went wrong</h1>}
+
+          {!loading && !error && organizations &&
+              <>
+                  <h1>Organizations</h1>
+                  <ul className='organization-list'>
+                      {organizations.map((orgItem, index) => {
+                          return (
+                              <OrganizationIcon
+                                  key={index}
+                                  id={index}
+                                  name={orgItem.name}
+                                  photo={orgItem.photo}
+                              />
+                          );
+                      })}
+                  </ul>
+              </>
+          }
       </div>
     </div>
   );
